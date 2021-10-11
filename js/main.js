4702b53b7f0a41ecc8714f60364f9fe2f042328e
Peregrin <peregrinirus@mail.ru>
2021-10-11 12:20:39 +0300

    Исправления в функциях рандома

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 function getRandomInteger(beginRange, endRange) {
+  if (beginRange<0 && endRange<0) {
+    return 'Неверные данные';
+  }
   if(beginRange>endRange){[beginRange, endRange] = [endRange, beginRange];}
+  if (beginRange<0) {
+    beginRange = 0;
+  }
+  if (endRange<0){
+    endRange = 0;
+  }
   beginRange = Math.ceil(beginRange);
   endRange = Math.floor(endRange);
-  return Math.floor(Math.random() * (endRange - beginRange +1))+ beginRange;
+  if (beginRange > endRange){
+    return 'Диапазон не содержит целочисленные значения';}
+  else {
+    return Math.floor(Math.random() * (endRange - beginRange +1))+ beginRange;
+  }
 }
 
 function getRandomArbitrary(beginRange, endRange, decimalPlaces) {
-
-  if(beginRange>endRange){[beginRange, endRange] = [endRange, beginRange];}
+  if (beginRange<0 && endRange<0){
+    return 'Неверные данные';
+  }
+  if (beginRange<0) {
+    beginRange = 0;
+  }
+  if (endRange<0){
+    endRange=0;
+  }
+  if (beginRange>endRange) {[beginRange, endRange] = [endRange, beginRange];}
   const flag = Math.round(Math.random());
 
   let arbitrary;
-  if (!flag) {
-    arbitrary = endRange - Math.random() * (endRange - beginRange);
-  } else {
-    arbitrary = Math.random() * (endRange - beginRange) + beginRange;
-  }
-  return arbitrary.toFixed(decimalPlaces);
+  flag === 0 ? arbitrary = Math.random() * (endRange - beginRange) + beginRange : arbitrary = endRange - Math.random() * (endRange - beginRange);
+  return parseFloat(arbitrary.toFixed(decimalPlaces));
 }
 
-getRandomInteger(6,6);
-getRandomArbitrary(1.3, 1.4, 3);
+getRandomInteger(-6, -1);
+getRandomArbitrary(-1.3, 1.4, 3);
