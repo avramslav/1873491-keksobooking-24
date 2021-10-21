28c75e9b36a7551b251be51189e5e81077ca83f7
Peregrin <peregrinirus@mail.ru>
2021-10-22 01:12:45 +0300

    Описание JS-объектов и начало функции их генерирования

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomInteger(beginRange, endRange) {
+const getRandomInteger = (beginRange, endRange) => {
   if (beginRange<0 && endRange<0) {
     return 'Неверные данные';
   }
@@ -18,7 +18,7 @@ function getRandomInteger(beginRange, endRange) {
   }
 }
 
-function getRandomArbitrary(beginRange, endRange, decimalPlaces) {
+const getRandomArbitrary = (beginRange, endRange, decimalPlaces) => {
   if (beginRange<0 && endRange<0){
     return 'Неверные данные';
   }
@@ -36,5 +36,57 @@ function getRandomArbitrary(beginRange, endRange, decimalPlaces) {
   return parseFloat(arbitrary.toFixed(decimalPlaces));
 }
 
-getRandomInteger(-6, -1);
-getRandomArbitrary(-1.3, 1.4, 3);
+console.log(getRandomInteger(-6, 13));
+console.log(getRandomArbitrary(-1.3, 1.4, 3));
+
+const checkinHours= ['12:00', '13:00', '14:00'];
+const checkoutHours=['12:00', '13:00', '14:00'];
+const typeOfHousing = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const arrayFeatures=[ 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const arrayPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+let temporaryData = {
+  author: {
+    avatar: 'img/avatars/user01.png',/* строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10.
+          Перед однозначными числами ставится 0. Например, 01, 02...10.
+          Адреса изображений не повторяются.*/
+  },
+  offer :{
+    title:'information about the ad',
+    address: '',
+    price: 0,
+    type:'',
+    rooms: 0,
+    guests: 0,
+    checkin:'',
+    checkout:'',
+    features:[],
+    description:'Удобный, чистый номер для отдыхающих',
+    photos:[]       /*массив строк — массив случайной длины из значений*/,
+    location:{    // объект — местоположение в виде географических координат. Состоит из двух полей:
+      lat:0.0,    /*число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000*/
+      lng: 0.0,   /*число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000*/
+
+    }
+
+
+
+  },
+};
+
+console.log(temporaryData);
+
+const getGeneratedObjects = () =>{
+  let avatarNumbers = [];
+  while(avatarNumbers.length < 10) {
+    let x = getRandomInteger(1,10);
+    if (!avatarNumbers.includes(x)) {
+      avatarNumbers.push(x);
+    }
+  }
+  avatarNumbers = avatarNumbers.map(addZero => 'img/avatars/user' + ('0'+addZero).substr(-2,2) + '.png')
+  return avatarNumbers;
+}
+console.log(getGeneratedObjects());
