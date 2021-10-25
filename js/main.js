5ee4cf70e7ab7244e6b3d7e0132e8d371d3581f1
Peregrin <peregrinirus@mail.ru>
2021-10-26 01:20:47 +0300

    Инициализация массива объектов JS

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const getRandomInteger = (beginRange, endRange) => {
   else {
     return Math.floor(Math.random() * (endRange - beginRange +1))+ beginRange;
   }
-}
+};
 
 const getRandomArbitrary = (beginRange, endRange, decimalPlaces) => {
   if (beginRange<0 && endRange<0){
@@ -34,10 +34,7 @@ const getRandomArbitrary = (beginRange, endRange, decimalPlaces) => {
   let arbitrary;
   flag === 0 ? arbitrary = Math.random() * (endRange - beginRange) + beginRange : arbitrary = endRange - Math.random() * (endRange - beginRange);
   return parseFloat(arbitrary.toFixed(decimalPlaces));
-}
-
-console.log(getRandomInteger(-6, 13));
-console.log(getRandomArbitrary(-1.3, 1.4, 3));
+};
 
 const checkinHours= ['12:00', '13:00', '14:00'];
 const checkoutHours=['12:00', '13:00', '14:00'];
@@ -47,46 +44,51 @@ const arrayPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
-let temporaryData = {
-  author: {
-    avatar: 'img/avatars/user01.png',/* строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10.
+let arrayTemporaryDates = Array(10);
+for (let i = 0; i < arrayTemporaryDates.length; i++){
+  arrayTemporaryDates[i] = {
+    author: {
+      avatar: '',/* строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10.
           Перед однозначными числами ставится 0. Например, 01, 02...10.
           Адреса изображений не повторяются.*/
-  },
-  offer :{
-    title:'information about the ad',
-    address: '',
-    price: 0,
-    type:'',
-    rooms: 0,
-    guests: 0,
-    checkin:'',
-    checkout:'',
-    features:[],
-    description:'Удобный, чистый номер для отдыхающих',
-    photos:[]       /*массив строк — массив случайной длины из значений*/,
-    location:{    // объект — местоположение в виде географических координат. Состоит из двух полей:
+    },
+    offer : {
+      title:'information about the ad',
+      address: '',
+      price: 0,
+      type:'',
+      rooms: 0,
+      guests: 0,
+      checkin:'',
+      checkout:'',
+      features:[],
+      description:'Удобный, чистый номер для отдыхающих',
+      photos:[] }      /*массив строк — массив случайной длины из значений*/,
+    location: {    // объект — местоположение в виде географических координат. Состоит из двух полей:
       lat:0.0,    /*число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000*/
       lng: 0.0,   /*число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000*/
 
-    }
+    },
+  };
+}
 
 
-
-  },
-};
-
-console.log(temporaryData);
-
-const getGeneratedObjects = () =>{
+const getGeneratedObjects = () => {
   let avatarNumbers = [];
   while(avatarNumbers.length < 10) {
-    let x = getRandomInteger(1,10);
+    const x = getRandomInteger(1,10);
     if (!avatarNumbers.includes(x)) {
       avatarNumbers.push(x);
     }
   }
-  avatarNumbers = avatarNumbers.map(addZero => 'img/avatars/user' + ('0'+addZero).substr(-2,2) + '.png')
-  return avatarNumbers;
-}
+  avatarNumbers = avatarNumbers.map((addZero) => `img/avatars/user${  (`0${addZero}`).substr(-2,2)  }.png`);
+  console.log(avatarNumbers);
+  arrayTemporaryDates = arrayTemporaryDates.map( (element,i) => {
+    element.author.avatar = avatarNumbers[i];
+    console.log(element);
+    return element;
+  });
+  //arrayTemporaryDates = arrayTemporaryDates.map(object => object[location].lat = 3;
+  return arrayTemporaryDates;
+};
 console.log(getGeneratedObjects());
